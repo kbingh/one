a35bb028831518341a9d5f5620fb26c1e6587619
kbing <kbing@ken_laptop>
2017-08-09 00:16:30 -0600

    # WARNING: head commit changed in the meantime
    initial commit for One

--- One/src/main/java/com/kb/one/security/Role.java
+++ One/src/main/java/com/kb/one/security/Role.java
@@ -1,9 +1,6 @@
 package com.kb.one.security;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 public class Role {
--- One/src/main/java/com/kb/one/security/User.java
+++ One/src/main/java/com/kb/one/security/User.java
@@ -1,5 +1,8 @@
 package com.kb.one.security;
 
+import javax.persistence.Entity;
+
+@Entity
 public class User {
 
 }
